c6567033a16d6197dd426a33d3ad106852eae7a5
Daniel Gaston <danielgo@graphcore.ai>
2022-06-10 11:40:58 +0100

    fpga/riscv Add code comments

--- fpga/include/mem.svh
+++ fpga/include/mem.svh
@@ -1,6 +1,15 @@
 `ifndef MEM_SVH
 `define MEM_SVH
 
+/**
+ * Data type to be read/write.
+ *
+ * BYTE and HALF will fill the upper 8/16 bytes with the result's MSB on
+ * read. UBYTE and UHALF will fill these bits with 0.
+ *
+ * On write, the BYTE/HALF/WORD will be written as it is, therefore
+ * BYTE is equivalent to UBYTE and HALF to UHALF in write ops.
+ */
 typedef enum bit [2:0]
 {
     MEM_DT_BYTE     = 3'b000,
