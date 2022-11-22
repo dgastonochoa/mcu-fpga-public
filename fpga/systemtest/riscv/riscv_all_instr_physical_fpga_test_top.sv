c8ec1f26056a062937dda878d4e55bdaa2930fd1
Daniel Gaston <danielgo@graphcore.ai>
2022-11-22 19:10:58 +0000

    fpga/riscv Bug fix: synchronized debounced GPIOs
    The MCU input GPIOs are debounced and then provided to the MCU. This causes setup
    violations. This possibly happens because, having that `clk` is `CLK100MHz` / 4, it
    could happen that the GPIO changes in the `CLK100MHz` rising edge that is just before
    the `clk` one, reducing the amount of time available to meet the hold violation to
    1/4 of the total. Fix this by synchronizing the debounced GPIOs with `clk`

--- fpga/systemtest/riscv/riscv_all_instr_physical_fpga_test_top.sv
+++ fpga/systemtest/riscv/riscv_all_instr_physical_fpga_test_top.sv
@@ -16,30 +16,24 @@ module riscv_all_instr_physical_fpga_test_top(
     input   wire        CLK100MHZ
 );
     wire rst;
-
     debounce_filter #(.WAIT_CLK(`DEBOUNCE_FILTER_WAIT_CLK)) df(
         btnC, CLK100MHZ, rst);
 
 
-    //
-    // Clock generation
-    //
     wire clk;
-
     clk_div #(.POL(1'd0), .PWIDTH(`CLK_PWIDTH)) cd(clk, CLK100MHZ, rst);
 
 
-    //
-    // RISC-V MCU
-    //
-    wire mosi, miso, ss, sck;
-    wire [7:0] JB_db;
-
+    wire [7:0] JB_db, JB_db_sync;
     debounce_filter #(.WAIT_CLK(`DEBOUNCE_FILTER_WAIT_CLK * 2)) df2 [7:0] (
         JB, CLK100MHZ, JB_db[7:0]);
+    cell_sync_n #(.N(8)) csn(clk, rst, JB_db, JB_db_sync);
 
+
+    wire mosi, miso, ss, sck;
     mcu #(.DEFAULT_INSTR(1), .SPI_SCK_WIDTH_CLKS(`SPI_SCK_PWIDTH)) m(
-        mosi, miso, ss, sck, JB_db, LED, rst, clk);
+        mosi, miso, ss, sck, JB_db_sync, LED, rst, clk);
+
 
     assign miso  = JA1[0];
     assign JA[3] = mosi;
