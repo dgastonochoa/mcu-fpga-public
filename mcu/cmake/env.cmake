c2f37a49ea77a4c35fe9b345f44801065fc797cb
Daniel Gaston Ochoa <dgastonochoa@gmail.com>
2024-03-29 22:45:31 +0000

    mcu Fix build bugs
    It seems changing to a new cmake version causes problems:
    
     - Include the env. part before using `project`, as the latter
       seems to need the environment set.
    
     - As a consequence of the above, don't use `CMAKE_PROJECT_DIR`
       when selecting the environment as this is unknown, since the
       project has not been declared yet.
    
     - Enable languages support only after the environment is set.

--- mcu/cmake/env.cmake
+++ mcu/cmake/env.cmake
@@ -1,9 +1,9 @@
 message(STATUS "Architecture = ${ARCH}")
 
 if(ARCH STREQUAL "arm")
-    include(${PROJECT_SOURCE_DIR}/cmake/toolchain-arm.cmake)
+    include(${CMAKE_SOURCE_DIR}/cmake/toolchain-arm.cmake)
 elseif(ARCH STREQUAL "riscv")
-    include(${PROJECT_SOURCE_DIR}/cmake/toolchain-riscv.cmake)
+    include(${CMAKE_SOURCE_DIR}/cmake/toolchain-riscv.cmake)
 else()
     message(FATAL_ERROR "Undefined architecure")
 endif()
--- mcu/cmake/toolchain-arm.cmake
+++ mcu/cmake/toolchain-arm.cmake
@@ -1,6 +1,3 @@
-enable_language(ASM)
-enable_language(C)
-
 # Set cross compilation information
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
@@ -19,10 +16,13 @@ set(CMAKE_OBJDUMP ${TOOLCHAIN_PREFIX}-objdump)
 set(CPU "-mcpu=cortex-m4")
 set(FPU "-mfpu=fpv4-sp-d16 -mfloat-abi=hard")
 set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -mthumb ${CPU}  ${FPU} -MD")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mthumb ${CPU} ${FPU} -std=c99 -Os -ffunction-sections -fdata-sections -MD -Wall -pedantic")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mthumb ${CPU} ${FPU}  -Os -ffunction-sections -fdata-sections -MD -Wall -pedantic -fno-exceptions -fno-rtti")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mthumb ${CPU} ${FPU} -std=c99 -Os -ffunction-sections -fdata-sections -MD -Wall -pedantic --specs=nosys.specs")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mthumb ${CPU} ${FPU}  -Os -ffunction-sections -fdata-sections -MD -Wall -pedantic -fno-exceptions -fno-rtti --specs=nosys.specs")
 
 set(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "")
 set(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS "")
 
 set(CMAKE_EXPORT_COMPILE_COMMANDS true)
+
+enable_language(ASM)
+enable_language(C)
